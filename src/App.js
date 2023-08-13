e59868810fc1cbae747a9d6175e106aa0948bae8
sebbyapudo <sebbyapudo@gmail.com>
2023-08-14 02:45:21 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function App() {
+  return (
+    <div className='container'>
+      <div className='row'>
+        <div className='col-6'>col number one</div>
+        <div className='col-6'>
+        <span><i className='fas fa-home'></i></span>
+        </div>
+        
+      </div>
+    </div>
+  );
+}
+
+export default App;
